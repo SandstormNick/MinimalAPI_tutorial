48bbe49b1a521e485e4c72c0a227f52a8803c486
NICHOLAS GIBBENS <GIBBENNX@legal.regn.net>
2023-04-11 23:17:54 +0200

    Adding TypedResults API example

---
+++ ToDoAPI/ToDoAPI/Program2.cs
@@ -0,0 +1,89 @@
+﻿//Use the TypedResults API
+
+//The Map<HttpVerb> methods can call route handler methods instead of using lambdas.
+//These methods return objects that implement IResult and are defined by TypedResults:
+
+//Unit tests can call these methods and test that they return the correct type. 
+
+//public async Task GetAllTodos_ReturnsOkOfTodosResult()
+//{
+//    // Arrange
+//    var db = CreateDbContext();
+
+//    // Act
+//    var result = await TodosApi.GetAllTodos(db);
+
+//    // Assert: Check for the correct returned type
+//    Assert.IsType<Ok<Todo[]>>(result);
+//}
+
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+var todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", GetAllTodos);
+todoItems.MapGet("/complete", GetCompleteTodos);
+todoItems.MapGet("/{id}", GetTodo);
+todoItems.MapPost("/", CreateTodo);
+todoItems.MapPut("/{id}", UpdateTodo);
+todoItems.MapDelete("/{id}", DeleteTodo);
+
+app.Run();
+
+static async Task<IResult> GetAllTodos(TodoDb db)
+{
+    return TypedResults.Ok(await db.Todos.ToArrayAsync());
+}
+
+static async Task<IResult> GetCompleteTodos(TodoDb db)
+{
+    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
+}
+
+static async Task<IResult> GetTodo(int id, TodoDb db)
+{
+    return await db.Todos.FindAsync(id)
+        is Todo todo
+            ? TypedResults.Ok(todo)
+            : TypedResults.NotFound();
+}
+
+static async Task<IResult> CreateTodo(Todo todo, TodoDb db)
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+}
+
+static async Task<IResult> UpdateTodo(int id, Todo inputTodo, TodoDb db)
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return TypedResults.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static async Task<IResult> DeleteTodo(int id, TodoDb db)
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return TypedResults.Ok(todo);
+    }
+
+    return TypedResults.NotFound();
+}
